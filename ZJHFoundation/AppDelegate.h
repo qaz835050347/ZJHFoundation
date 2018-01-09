d81edf48cf666de05c76922dfff8973f22e33a47
曾俊晖 <835050347@qq.com>
2018-01-09 09:30:22 +0800

    提交项目

---
+++ ZJHFoundation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJHFoundation
+//
+//  Created by 曾俊晖 on 2018/1/9.
+//  Copyright © 2018年 ZJH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
